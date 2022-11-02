24f9c3430a70737a01348eb2e95559da6f3b0bf3
Maricus Samson <macadadi36@gmail.com>
2022-11-02 20:47:45 +0300

    Add relations

--- form/productForm.go
+++ form/productForm.go
@@ -3,9 +3,9 @@ package form
 import "time"
 
 type ProductForm struct{
+	Id   int64  	`json:"id" `
 	Name string   `json:"name" binding:"required"`
 	Price int64  	`json:"price" binding:"required"`
-	Id   int64  	`json:"id" `
 	User_id int64  	`json:"user_id"`
 }
 
